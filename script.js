9f2a5606f7da733fed4c3ccba994501489f42079
yirano <tiffany.arellano@outlook.com>
2019-02-04 16:53:36 -0800

    format document

--- script.js
+++ script.js
@@ -3,19 +3,18 @@ var body = document.querySelector('body');
 var header = document.querySelector('header');
 var scrollTitle = document.getElementById('scrollTitle');
 
-document.addEventListener("DOMContentLoaded", function(event) {
-    
-    window.addEventListener('scroll', function(){
-        
-        if (window.pageYOffset >= 100) {
-            scrollTitle.classList.add('scrollThisAnim');
-            scrollTitle.classList.remove('scrollUp');
-        } else if (window.pageYOffset > 0 && window.pageYOffset < 100){
-            scrollTitle.style.display ='block';
+document.addEventListener("DOMContentLoaded", function (event) {
 
+    window.addEventListener('scroll', function () {
+
+        if (window.pageYOffset >= 100) {
+            scrollTitle.classList.add('scrollDown');
+            scrollTitle.classList.remove('scrollUp');
+        } else if (window.pageYOffset > 0 && window.pageYOffset < 100) {
+            scrollTitle.style.visual = 'hidden';
         } else {
-            scrollTitle.classList.remove('scrollThisAnim');
+            scrollTitle.classList.remove('scrollDown');
             scrollTitle.classList.add('scrollUp');
         }
     });
-  });
+});
