0e6c4a8f8feaaea82909036c87fa15b4c5c39028
yirano <tiffany.arellano@outlook.com>
2019-02-04 20:46:38 -0800

    correct header in nav action on scroll

--- script.js
+++ script.js
@@ -3,18 +3,20 @@ var body = document.querySelector('body');
 var header = document.querySelector('header');
 var scrollTitle = document.getElementById('scrollTitle');
 
-document.addEventListener("DOMContentLoaded", function (event) {
+document.addEventListener("DOMContentLoaded", function(event) {
 
-    window.addEventListener('scroll', function () {
-
-        if (window.pageYOffset >= 100) {
+    window.addEventListener('scroll', function(){
+        
+        if (window.pageYOffset > 150) {
             scrollTitle.classList.add('scrollDown');
             scrollTitle.classList.remove('scrollUp');
-        } else if (window.pageYOffset > 0 && window.pageYOffset < 100) {
-            scrollTitle.style.visual = 'hidden';
-        } else {
+        } else  {
             scrollTitle.classList.remove('scrollDown');
             scrollTitle.classList.add('scrollUp');
+
+            console.log("scrollUp");
         }
     });
+
+    
 });
