d7bc87d898d1a4946ba17d81e598fe6af491edf9
yirano <tiffany.arellano@outlook.com>
2019-02-07 11:28:56 -0800

    add scroll-up arrow in nav + hover border

--- script.js
+++ script.js
@@ -2,21 +2,31 @@ var nav = document.querySelector('nav');
 var body = document.querySelector('body');
 var header = document.querySelector('header');
 var scrollTitle = document.getElementById('scrollTitle');
+var scrollArrowHeader = document.getElementById('scrollArrowHeader');
 
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", function (event) {
 
-    window.addEventListener('scroll', function(){
-        
+    window.addEventListener('scroll', function () {
+
         if (window.pageYOffset > 150) {
             scrollTitle.classList.add('scrollDown');
             scrollTitle.classList.remove('scrollUp');
-        } else  {
+            scrollArrowHeader.classList.add('scrollArrDown');
+            scrollArrowHeader.classList.remove('scrollUp');
+        } else {
             scrollTitle.classList.remove('scrollDown');
             scrollTitle.classList.add('scrollUp');
-
-            console.log("scrollUp");
+            scrollArrowHeader.classList.remove('scrollArrDown');
+            scrollArrowHeader.classList.add('scrollUp');
         }
     });
 
-    
+    var firstSub = document.querySelector('.subA');
+
+    firstSub.addEventListener('mouseover', function(){
+        console.log("entered");
+    });
+
+    console.log(window.innerHeight);
+
 });
