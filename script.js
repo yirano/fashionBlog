724edb2802a1dccd6ce9e916d14d6e03a4795303
yirano <tiffany.arellano@outlook.com>
2019-02-09 19:17:04 -0800

    fade header on scroll

--- script.js
+++ script.js
@@ -8,16 +8,23 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
     window.addEventListener('scroll', function () {
 
-        if (window.pageYOffset > 150) {
+        if (window.pageYOffset > 100) {
             scrollTitle.classList.add('scrollDown');
             scrollTitle.classList.remove('scrollUp');
             scrollArrowHeader.classList.add('scrollArrDown');
             scrollArrowHeader.classList.remove('scrollUp');
+            header.classList.add('fadeHeader');
+            header.classList.remove('showHeader');
+
         } else {
             scrollTitle.classList.remove('scrollDown');
             scrollTitle.classList.add('scrollUp');
             scrollArrowHeader.classList.remove('scrollArrDown');
             scrollArrowHeader.classList.add('scrollUp');
+            header.classList.remove('fadeHeader');
+            header.classList.add('showHeader');
+
+
         }
     });
 
