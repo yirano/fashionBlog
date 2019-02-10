6761e73ea8c4adc18ce807bfd6b2aa186103a81e
yirano <tiffany.arellano@outlook.com>
2019-02-09 20:15:31 -0800

    slide in sidebars into view on scroll and alternate

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ var body = document.querySelector('body');
 var header = document.querySelector('header');
 var scrollTitle = document.getElementById('scrollTitle');
 var scrollArrowHeader = document.getElementById('scrollArrowHeader');
+var rightBar = document.querySelector('.rightBar');
+var leftBar = document.querySelector('.leftBar');
 
 document.addEventListener("DOMContentLoaded", function (event) {
 
@@ -15,7 +17,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
             scrollArrowHeader.classList.remove('scrollUp');
             header.classList.add('fadeHeader');
             header.classList.remove('showHeader');
-
         } else {
             scrollTitle.classList.remove('scrollDown');
             scrollTitle.classList.add('scrollUp');
@@ -23,9 +24,24 @@ document.addEventListener("DOMContentLoaded", function (event) {
             scrollArrowHeader.classList.add('scrollUp');
             header.classList.remove('fadeHeader');
             header.classList.add('showHeader');
-
-
         }
+
+        if (window.pageYOffset > 1270) {
+            rightBar.classList.remove('rightBarHide');
+            rightBar.classList.add('rightBarShow');
+
+            leftBar.classList.remove('leftBarHide');
+            leftBar.classList.add('leftBarShow');
+
+        } else {
+            rightBar.classList.add('rightBarHide');
+            rightBar.classList.remove('rightBarShow');
+
+            leftBar.classList.add('leftBarHide');
+            leftBar.classList.remove('leftBarShow');
+        }
+
+        console.log(pageYOffset);
     });
 
     var firstSub = document.querySelector('.subA');
