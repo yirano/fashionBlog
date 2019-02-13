3ef4449db561f6c92c5c6b60da0fc0fe01d3797b
yirano <tiffany.arellano@outlook.com>
2019-02-12 19:31:31 -0800

    add social icons in blog.html + change fontsize + rearrange stylesheet

--- script.js
+++ script.js
@@ -8,48 +8,111 @@ var leftBar = document.querySelector('.leftBar');
 
 document.addEventListener("DOMContentLoaded", function (event) {
 
+        console.log(location.href);
+        console.log(document.URL);
+
     window.addEventListener('scroll', function () {
 
-        if (window.pageYOffset > 100) {
-            scrollTitle.classList.add('scrollDown');
-            scrollTitle.classList.remove('scrollUp');
-            scrollArrowHeader.classList.add('scrollArrDown');
-            scrollArrowHeader.classList.remove('scrollUp');
-            header.classList.add('fadeHeader');
-            header.classList.remove('showHeader');
+        if (document.URL.contains == "blog.html") {
+            if (window.pageYOffset > 100) {
+                scrollTitle.classList.add('scrollDown');
+                scrollTitle.classList.remove('scrollUp');
+                scrollArrowHeader.classList.add('scrollArrDown');
+                scrollArrowHeader.classList.remove('scrollUp');
+                header.classList.add('fadeHeader');
+                header.classList.remove('showHeader');
+            } else {
+                scrollTitle.classList.remove('scrollDown');
+                scrollTitle.classList.add('scrollUp');
+                scrollArrowHeader.classList.remove('scrollArrDown');
+                scrollArrowHeader.classList.add('scrollUp');
+                header.classList.remove('fadeHeader');
+                header.classList.add('showHeader');
+            }
+    
+            if (window.pageYOffset > 270) {
+                rightBar.classList.remove('rightBarHide');
+                rightBar.classList.add('rightBarShow');
+    
+                leftBar.classList.remove('leftBarHide');
+                leftBar.classList.add('leftBarShow');
+    
+            } else {
+                rightBar.classList.add('rightBarHide');
+                rightBar.classList.remove('rightBarShow');
+    
+                leftBar.classList.add('leftBarHide');
+                leftBar.classList.remove('leftBarShow');
+            }
         } else {
-            scrollTitle.classList.remove('scrollDown');
-            scrollTitle.classList.add('scrollUp');
-            scrollArrowHeader.classList.remove('scrollArrDown');
-            scrollArrowHeader.classList.add('scrollUp');
-            header.classList.remove('fadeHeader');
-            header.classList.add('showHeader');
+            if (window.pageYOffset > 100) {
+                scrollTitle.classList.add('scrollDown');
+                scrollTitle.classList.remove('scrollUp');
+                scrollArrowHeader.classList.add('scrollArrDown');
+                scrollArrowHeader.classList.remove('scrollUp');
+                header.classList.add('fadeHeader');
+                header.classList.remove('showHeader');
+            } else {
+                scrollTitle.classList.remove('scrollDown');
+                scrollTitle.classList.add('scrollUp');
+                scrollArrowHeader.classList.remove('scrollArrDown');
+                scrollArrowHeader.classList.add('scrollUp');
+                header.classList.remove('fadeHeader');
+                header.classList.add('showHeader');
+            }
+    
+            if (window.pageYOffset > 1270) {
+                rightBar.classList.remove('rightBarHide');
+                rightBar.classList.add('rightBarShow');
+    
+                leftBar.classList.remove('leftBarHide');
+                leftBar.classList.add('leftBarShow');
+    
+            } else {
+                rightBar.classList.add('rightBarHide');
+                rightBar.classList.remove('rightBarShow');
+    
+                leftBar.classList.add('leftBarHide');
+                leftBar.classList.remove('leftBarShow');
+            }
         }
-
-        if (window.pageYOffset > 1270) {
-            rightBar.classList.remove('rightBarHide');
-            rightBar.classList.add('rightBarShow');
-
-            leftBar.classList.remove('leftBarHide');
-            leftBar.classList.add('leftBarShow');
-
-        } else {
-            rightBar.classList.add('rightBarHide');
-            rightBar.classList.remove('rightBarShow');
-
-            leftBar.classList.add('leftBarHide');
-            leftBar.classList.remove('leftBarShow');
-        }
-
-        console.log(pageYOffset);
+    
     });
 
-    var firstSub = document.querySelector('.subA');
 
-    firstSub.addEventListener('mouseover', function () {
-        console.log("entered");
-    });
-
-    console.log(window.innerHeight);
 
 });
+
+function pageAnim() {
+    if (window.pageYOffset > 100) {
+        scrollTitle.classList.add('scrollDown');
+        scrollTitle.classList.remove('scrollUp');
+        scrollArrowHeader.classList.add('scrollArrDown');
+        scrollArrowHeader.classList.remove('scrollUp');
+        header.classList.add('fadeHeader');
+        header.classList.remove('showHeader');
+    } else {
+        scrollTitle.classList.remove('scrollDown');
+        scrollTitle.classList.add('scrollUp');
+        scrollArrowHeader.classList.remove('scrollArrDown');
+        scrollArrowHeader.classList.add('scrollUp');
+        header.classList.remove('fadeHeader');
+        header.classList.add('showHeader');
+    }
+
+    if (window.pageYOffset > 1270) {
+        rightBar.classList.remove('rightBarHide');
+        rightBar.classList.add('rightBarShow');
+
+        leftBar.classList.remove('leftBarHide');
+        leftBar.classList.add('leftBarShow');
+
+    } else {
+        rightBar.classList.add('rightBarHide');
+        rightBar.classList.remove('rightBarShow');
+
+        leftBar.classList.add('leftBarHide');
+        leftBar.classList.remove('leftBarShow');
+    }
+}
+    
